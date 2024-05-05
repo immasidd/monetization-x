7747240cc823ae88d4aae2a1244b2f2263dee7ca
Siddhant Choudhary <67625352+immasidd@users.noreply.github.com>
2024-05-05 20:59:41 +0530

    Upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThreeDCardDemo } from "./AceternityUI/3D-cardEffect/3d-cardComponent";
+
+function App() {
+
+  return (
+    <>
+      <ThreeDCardDemo></ThreeDCardDemo>
+    </>
+  )
+}
+
+export default App
